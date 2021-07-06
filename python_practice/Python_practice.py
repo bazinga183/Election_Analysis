616f2483c37fd5662b859da700b12507fd06af58
Eduardo Flores <eddie.flores183@gmail.com>
2021-07-06 12:33:38 -0500

    Adding PyPoll.py file

---
+++ python_practice/Python_practice.py
@@ -0,0 +1,10 @@
+Pressley_info = {
+    "name" : "Dweebus the Third",
+    "age" : "Thankfully not a minor",
+    "hobbies" : ["Embroidering", "Reading Good Books", "Publishing a Thesis", "Smoking that Gonja"],
+    "wake-up" : {"Mon" : 8, "Tue" : 8, "Wed" : 8}
+}
+print(f'Hello, I am {Pressley_info["name"]} and I am {Pressley_info["age"]} years old')
+print(f'I have {len(Pressley_info["hobbies"])} hobbies')
+print(f'My favorite hobby is {Pressley_info["hobbies"][3]}')
+print(f'On Monday, I like waking up at {Pressley_info["wake-up"]["Tue"]} AM.')
